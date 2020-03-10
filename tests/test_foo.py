00950667bb0a47844a66cf880fd07eb7010fdd17
Jason Verbeek <jason@localhost8080.org>
2020-03-10 21:52:29 +0100

    keyword

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -6,7 +6,7 @@ class FooTest(TestCase):
 
     def test_convert_num(self):
         milligrams = 2578000
-        grams = convert_num(milligrams, step=1000)
+        grams = convert_num(num=milligrams, step=1000)
         kilograms_from_gr = convert_num(num=grams, step=1000)
         kilograms_from_mg = convert_num(num=milligrams, step=10**6)
         self.assertEqual(kilograms_from_gr, kilograms_from_mg)
